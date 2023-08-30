b3fa329d73695239f4786d7a31d6836486aa4d6c
Reinaldo Alves <102365824+Kingnaldo1337@users.noreply.github.com>
2023-08-30 12:32:18 -0300

    Nova versão

--- Cigana.C
+++ Cigana.C
@@ -1,272 +1,47 @@
-///////////////////////////////////////////////////////////////////////////////
-///             Universidade Federal do Rio Grande do Norte                 ///
-///                 Centro de Ensino Superior do Seridó                     ///
-///               Departamento de Computação e Tecnologia                   ///
-///                  Disciplina DCT1106 -- Programação                      ///
-///                  Projeto Sistema de Cigana                              ///
-///             Developed by @r3inaldo_jr -- since Aug, 2023                ///
-///////////////////////////////////////////////////////////////////////////////
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include "cigana.h"
 
-void menu_principal(void);
-void sobre(void);
-void menu_cigana(void);
-void menu_cliente(void);
-void menu_adivinhacoes(void);
-void menu_financeiro(void);
-void menu_amoroso(void);
-void menu_emocional(void);
-void menu_profissional(void);
+void limpartela(){
+	#ifdef _WIN32
+  	  system("cls");
+	#else
+  	  system("clear");
+	#endif
+}
 
-int main(void){
-    menu_principal();
-    sobre();
-    menu_cigana();
+void continuar(){
+	printf("Pressione enter para continuar...");
+	getchar();
+}
+
+void leituraDeCartas() {
+    // Implemente a função de leitura de cartas de tarô aqui
+}
+
+void horoscopoDiario() {
+    const char* signos[12] = {
+        "Áries", "Touro", "Gêmeos", "Câncer", "Leão", "Virgem",
+        "Libra", "Escorpião", "Sagitário", "Capricórnio", "Aquário", "Peixes"
+    };
     
-    menu_principal();
-    sobre();
-   
-
-    return 0;
-}
-
-void menu_principal(void){
-    char op;
-    system("clear||cls");
-     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
-    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
-    printf("///               Departamento de Computação e Tecnologia                   ///\n");
-    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
-    printf("///                  Projeto Sistema de Cigana                              ///\n");
-    printf("///             Developed by @r3inaldo_jr -- since Aug, 2023                ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///                 = = = = = Sistema de Cigana = = = = =                   ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            1. Módulo Cigana                                             ///\n");
-    printf("///            2. Módulo Cliente                                            ///\n");
-    printf("///            3. Módulo Adivingacoes                                       ///\n");
-    printf("///            4. Módulo Sobre                                              ///\n");
-    printf("///            0. Sair                                                      ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            Escolha a opção desejada: ");
-    scanf("%c", &op);
-    getchar();
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-    getchar();
-}
-
-void sobre(void){
-    system("clear||cls");
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
-    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
-    printf("///               Departamento de Computação e Tecnologia                   ///\n");
-    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
-    printf("///                  Projeto Sistema de Cigana                              ///\n");
-    printf("///             Developed by @r3inaldo_jr -- since Aug, 2023                ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///                 = = = = = Sistema de Cigana = = = = =                   ///\n");
-    printf("///                                                                         ///\n");
-    printf("///  Projeto Utilizado para simular uma Cigana Virtual, onde poderemos      ///\n");
-    printf("///  ter funções como adivinhar futuro amoroso, financeiro, profissinal     ///\n");
-    printf("///  dentre outros.                                                         ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-    getchar();
-}
-
-    void menu_cigana(void) {
+    const char* horoscopos[7] = {
+        "Hoje é um dia de sorte no trabalho. Aproveite as oportunidades que surgirem.",
+        "Seja cuidadoso nas suas decisões financeiras hoje. Evite gastos desnecessários.",
+        "Sua energia e criatividade estão em alta. É um bom momento para atividades sociais.",
+        "É hora de cuidar da sua saúde. Mantenha uma alimentação equilibrada e pratique exercícios.",
+        "Este é um dia favorável para investir em relacionamentos. Mostre seu apoio aos amigos.",
+        "Fique atento às oportunidades de aprendizado. Novos conhecimentos estão ao seu alcance.",
+        "Hoje é um bom dia para relaxar e cuidar de si mesmo. Tire um tempo para o autocuidado."
+    };
     
-   
-char op;
-    system("clear||cls");
-    printf("\n");
-    
-   
-printf("=====================================\n");
-    printf("   Sistema de Adivinhação de Futuros  \n");
-    printf("=====================================\n");
-    printf("1. Adivinhar futuro Financeiro\n");
-    printf("2. Adivinhar futuro Amoroso\n");
-    printf("3. Adivinhar futuro Emocional\n");
-    printf("4. Adivinhar futuro Profissional\n");
-    printf("0. Voltar ao menu anterior\n");
-    printf("=====================================\n");
-    printf("Escolha a opção desejada: ");
-    scanf(" %c", &op);
-    getchar();
-    printf("=====================================\n");
-    printf("\n");
-    
-   
-printf(">>> Tecle <ENTER> para continuar...\n");
-    getchar();
+    int signoIndex = rand() % 12;
+    int horoscopoIndex = rand() % 7;
 
-    
-    getchar();
-
-switch (op) {
-        case '1':
-            menu_financeiro();
-            
-            menu
-break;
-        case '2':
-            menu_amoroso();
-            
-            menu_am
-break;
-        case '3':
-            menu_emocional();
-            
-            menu_emocional();
-           
-
-           
-break;
-        case '4':
-            menu_profissional();
-            
-            menu_profissional();
-           
-break;
-        case '0':
-            // Handle the option to go back to the previous menu
-            
-           
-break;
-        default:
-            // Handle invalid input
-            break;
-    }
+    printf("Horóscopo para %s:\n%s\n", signos[signoIndex], horoscopos[horoscopoIndex]);
 }
 
-
-    void menu_financeiro(void){
-    system("clear||cls");
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            = = = =  Sistema de Adivinhação de Futuros  = = = =          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///               Developed by @r3inaldo_jr -- since Aug, 2023              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///            = = = = = = = = Futuro Financeiro = = = = = = = =            ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            Cpf do cliente (apenas números):                             ///\n");
-    printf("///            Signo:                                                       ///\n");
-    printf("///            Religião:                                                    ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-    getchar();
+void sobreSeuSigno() {
+    // Implemente a função que fala sobre o signo aqui
 }
-        void menu_Amoroso(void){
-    system("clear||cls");
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            = = = =  Sistema de Adivinhação de Futuros  = = = =          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///               Developed by @r3inaldo_jr -- since Aug, 2023              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///            = = = = = = = = Futuro Amoroso = = = = = = = =               ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            Cpf do cliente (apenas números):                             ///\n");
-    printf("///            Signo:                                                       ///\n");
-    printf("///            Religião:                                                    ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-    getchar();
-}
-        void menu_emocional(void){
-    system("clear||cls");
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            = = = =  Sistema de Adivinhação de Futuros  = = = =          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///               Developed by @r3inaldo_jr -- since Aug, 2023              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///            = = = = = = = = Futuro Emocional = = = = = = = =             ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            Cpf do cliente (apenas números):                             ///\n");
-    printf("///            Signo:                                                       ///\n");
-    printf("///            Religião:                                                    ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-    getchar();
-}
-        void menu_profissinal(void){
-    system("clear||cls");
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            = = = =  Sistema de Adivinhação de Futuros  = = = =          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///               Developed by @r3inaldo_jr -- since Aug, 2023              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///            = = = = = = = = Futuro profissional = = = = = = = =          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            Cpf do cliente (apenas números):                             ///\n");
-    printf("///            Signo:                                                       ///\n");
-    printf("///            Religião:                                                    ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-    getchar();
-    }
